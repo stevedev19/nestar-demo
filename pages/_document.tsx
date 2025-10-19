b33e6e6217da5d43762e41c41dfee2ec40ac6a12
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-10-19 20:24:35 +0900

    feat: MUI customized integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log("DOCUMENT PAGES ROUTER");
+  
 
   return (
     <Html lang="en">
-      <Head />
+      <Head>
        <meta charSet="UTF-8 " />
         <meta name="title" content="Nestar Demo" />
         <meta name="robots" content="index, follow" />
@@ -17,6 +17,7 @@ export default function Document() {
           "Buy and sell properties anywhere anytime in South Korea. | " +
           "Покупайте и продавайте недвижимость в любое время и в любом месте по всей Южной Корее. | " +
           "언제 어디서든 대한민국의 부동산을 사고파세요."} />
+      </Head>
       <body>
         <Main />
         <NextScript />
