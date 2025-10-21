3e611514c72341d30001956ca5c1c6e0b0c52244
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-10-21 20:53:41 +0900

    feat: SASS integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import {light} from "../scss/MaterialTheme"
+import {light} from "../scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
  // @ts-ignore
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,20 @@ import withLayoutMain from "@/libs/components/layout/LayoutHome";
 import { Box, Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 
-  const Home: NextPage=()=>{
+  const Home: NextPage=() => {
   return (
-    <Container>
+    
     <Stack flexDirection={"column"}>
-      <Box>Popular Properties</Box>
-      <Box>Top Agents</Box>
-      <Box>Top Properties</Box>
-      <Box>Events</Box>
+      <Stack>
+        <Stack className="container" >Popular Properties</Stack>
+        </Stack>
+        <Stack>
+        <Stack className="container" >Top Agents</Stack>
+        </Stack>
+        <Stack>
+        <Stack className="container" >Top Properties</Stack>
+        </Stack>
     </Stack>
-</Container>
   )
 }
 
