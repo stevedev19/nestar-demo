ebbdc85237fa66f962becad82d34a5f222b55aeb
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-10-22 06:17:51 +0900

    feat: publish screen component homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,25 @@
+import Advertisement from "@/libs/components/homepage/Advertisement";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import TopAgents from "@/libs/components/homepage/TopAgents";
+import TopProperties from "@/libs/components/homepage/TopProperties";
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import { Box, Container, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-  const Home: NextPage=() => {
+  const Home: NextPage= () => {
   return (
-  <Stack>
-    <Stack flexDirection={"column"}>
-      <Stack>
-        <Stack className="container" >Popular Properties</Stack>
-        </Stack>
-        <Stack>
-        <Stack className="container" >Top Agents</Stack>
-        </Stack>
-        <Stack>
-        <Stack className="container" >Top Properties</Stack>
-        </Stack>
+   <Stack className="home-page">
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
     </Stack>
-  </Stack>
-  )
-}
+  );
+};
 
 export default withLayoutMain(Home);
