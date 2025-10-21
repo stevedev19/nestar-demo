63ba553304f5e0f62d1dded657aee9840eb46b67
stevedev19 <abdulla.toshtemirov17eng@gmail.com>
2025-10-22 01:49:13 +0900

    feat: publish basic layout components

--- libs/components/layout/LayoutBasic.tsx
+++ libs/components/layout/LayoutBasic.tsx
@@ -1,29 +1,45 @@
-import { Stack } from "@mui/material"
-//import {Head}from "next/document"
-import Head from "next/head"
+import { Stack } from "@mui/material";
+import Head from "next/head";
+import Top from "../Top";
+import Footer from "../Footer";
 
-const withLayoutBasic = (Component: any)=>{
+const withLayoutBasic = (Component: any) => {
+  return (props: any) => {
+    return (
+      <>
+        <Head>
+          <title>Nestar</title>
+        </Head>
+        <Stack id="pc-wrap">
+          <Stack id={"top"}>
+            <Top />
+          </Stack>
 
-//const withLayoutBasic = (Component: React.ComponentType<any>) => {
-
-    return (props: any) => { 
-        return (
-             <>
-            <Head>  
-                <title>Nestar</title>
-            </Head>
-            <Stack id="pc-wrap">
-            <Stack sx={{background: "#81c784"}}>Header Basic</Stack>
-            <Stack id={"main"}>
-              <Component {...props}/>
+          <Stack
+            className="header-basic"
+            style={{
+              backgroundImage: `url(/img/banner/properties.png)`,
+              backgroundSize: "cover",
+              boxShadow: "inset 10px 40px 50px 40px rgb(24 22 36",
+            }}
+          >
+            <Stack className="container">
+              <strong>Property Search</strong>
+              <span>We are glad to see you again!</span>
+            </Stack>
             </Stack>
 
-            <Stack sx={{background: "#a1887f"}}>Footer</Stack>
-            </Stack>
-            </>
-        )
-       
-    };
+          <Stack id={"main"}>
+            <Component {...props} />
+          </Stack>
+
+          <Stack id={"footer"}>
+            <Footer />
+          </Stack>
+        </Stack>
+      </>
+    );
+  };
 };
 
 export default withLayoutBasic;
